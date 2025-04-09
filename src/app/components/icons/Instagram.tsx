2259aed9f4caffdf5573dabca7e81fe96d23fc68
giorgi gogichaishvili <g.dodicha@gmail.com>
2025-04-10 00:25:50 +0400

    starting to make portfolio

---
+++ src/app/components/icons/Instagram.tsx
@@ -0,0 +1,15 @@
+import { FaInstagram } from "react-icons/fa";
+
+
+export default function InstagramIcon() {
+  return (
+    <a
+      href="https://www.instagram.com/gogichaishvili_g/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-500 hover:text-blue-600 transition-colors text-4xl"
+    >
+      <FaInstagram />
+    </a>
+  );
+}
