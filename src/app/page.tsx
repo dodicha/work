12f6a3274c61d8a0fe18a0710f0766a9f7dda4ce
giorgi gogichaishvili <g.dodicha@gmail.com>
2025-04-15 22:27:56 +0400

    finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,48 +4,8 @@ import { FaEnvelope, FaMobileAlt } from "react-icons/fa";
 import Image from "next/image";
 
 export default function Home() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
-    };
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => window.removeEventListener("mousemove", handleMouseMove);
-  }, []);
-
   return (
     <div className="relative overflow-hidden h-screen w-full bg-gradient-to-b from-[#07071b] to-[#0d0d5e] -z-10">
-      {/* Background Figures */}
-      <div className="absolute inset-0 -z-10">
-        <div
-          className=" w-[70px] h-[50px] border-2 border-red-400 top-10 right-10 mt-[50px]"
-          style={{
-            borderTopLeftRadius: "100px",
-            borderTopRightRadius: "100px",
-            transform: `translate(${mousePosition.x * 0.02}px, ${
-              mousePosition.y * 0.05
-            }px)`,
-          }}
-        ></div>
-        <div
-          className="absolute w-[100px] h-[60px] border-green-400 border-2 rounded-full top-60 left-1/2"
-          style={{
-            transform: `translate(${mousePosition.x * 0.03}px, ${
-              mousePosition.y * 0.03
-            }px)`,
-          }}
-        ></div>
-        <div
-          className="absolute w-[50px] h-[50px] bg-purple-400 rounded-full bottom-20 left-10"
-          style={{
-            transform: `translate(${mousePosition.x * 0.04}px, ${
-              mousePosition.y * 0.04
-            }px)`,
-          }}
-        ></div>
-      </div>
-
       {/* Content */}
       <div className="flex items-center justify-center h-full mx-auto text-[#a9a9e2]">
         <div className="text-center w-[597px]">
@@ -56,7 +16,7 @@ export default function Home() {
             </b>
           </h1>
           <h2 className="font-serif text-4xl mt-[40px]">
-            <b> FRONTEND DEVELOPER </b> <br></br> FROM GEORGIA
+            <b> WEB DEVELOPER </b> <br></br> FROM GEORGIA
           </h2>
 
           <div className="flex items-center justify-center mt-[30px] gap-8">
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,31 +1,48 @@
-import React from 'react'
-import Link from 'next/link'
-import FacebookIcon from '../icons/Facebook'
-import InstagramIcon from '../icons/Instagram'
+import React from "react";
+import Link from "next/link";
+import FacebookIcon from "../icons/Facebook";
+import InstagramIcon from "../icons/Instagram";
 
 export default function Navigation() {
   return (
     <div>
       <div className="w-[220px] h-screen flex items-center justify-center  bg-gradient-to-b from-[#07071b] to-[#0d0d5e]">
-          <div className=" w-[150px] text-white ">
-            
-            <h3 className="font-serif font-bold text-5xl">G.G</h3>
+        <div className=" w-[150px] text-white ">
+          <h3 className="font-serif font-bold text-5xl cursor-pointer">G.G</h3>
 
-            <ul className="flex flex-col mt-[210px] font-neue gap-[20px] font-bold text-xl">
-              <Link href="/" className="transition-all duration-300 hover:scale-110">HOME</Link>
-              <Link href="/about" className="transition-all duration-300 hover:scale-110">ABOUT</Link>
-              <Link href="/work" className="transition-all duration-300 hover:scale-110">WORKS</Link>
-              <Link href="/contact" className="transition-all duration-300 hover:scale-110">CONTACT</Link>
-            </ul>
+          <ul className="flex flex-col mt-[210px] font-neue gap-[30px] font-bold text-xl">
+            <Link
+              href="/"
+              className="transition-all duration-300 hover:scale-110"
+            >
+              HOME
+            </Link>
+            <Link
+              href="/about"
+              className="transition-all duration-300 hover:scale-110"
+            >
+              ABOUT
+            </Link>
+            <Link
+              href="/work"
+              className="transition-all duration-300 hover:scale-110"
+            >
+              WORKS
+            </Link>
+            <Link
+              href="/contact"
+              className="transition-all duration-300 hover:scale-110"
+            >
+              CONTACT
+            </Link>
+          </ul>
 
-            <div className="flex flex-col mt-[200px] gap-4">
-              <InstagramIcon />
-              <FacebookIcon />
-            </div>
-
-              
+          <div className="flex flex-col mt-[200px] gap-4">
+            <InstagramIcon />
+            <FacebookIcon />
           </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
