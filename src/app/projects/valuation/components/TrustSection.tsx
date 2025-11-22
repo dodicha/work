3d08ef117249ee5c83fc4d6d2f2dad3203d2b79f
giorgi gogichaishvili <g.dodicha@gmail.com>
2025-11-22 06:31:24 +0400

    created fast valuation section

--- src/app/projects/valuation/components/TrustSection.tsx
+++ src/app/projects/valuation/components/TrustSection.tsx
@@ -2,14 +2,14 @@
 
 import React from "react";
 import { useLanguage } from "../context/LanguageProvider";
-import { Award, Building2 } from "lucide-react";
+import { Award, Building2, BadgeCheck, Star, Handshake } from "lucide-react";
 
 export default function TrustSection() {
   const { selectedLanguageData } = useLanguage();
   return (
-    <section className=" w-4/5 ml-[70px]">
+    <section className=" w-4/5 ml-[50px]  mr-4 flex flex-wrap gap-8 p-24">
       {/* 1 */}
-      <div className="flex flex-col items-center text-center ">
+      <div className="flex flex-col items-center text-center w-1/4 ">
         <Building2 className="text-blue-500 w-10 h-10 mb-3" />
         <h3 className="text-xl font-semibold">35,000+</h3>
         <p className="text-gray-500 text-sm">
@@ -17,7 +17,7 @@ export default function TrustSection() {
         </p>
       </div>
       {/* 2 */}
-      <div className="flex flex-col items-center text-center ">
+      <div className="flex flex-col items-center text-center w-1/4">
         <Award className="text-blue-500 w-10 h-10 mb-3" />
         <h3 className="text-xl font-semibold">
           17+ <span>{selectedLanguageData?.["years"]}</span>{" "}
@@ -26,10 +26,33 @@ export default function TrustSection() {
           {selectedLanguageData?.["of work experience"]}
         </p>
       </div>
+      <div className="flex flex-col items-center text-center w-1/4">
+        <Building2 className="text-blue-500 w-10 h-10 mb-3" />
+        <h3 className="text-xl font-semibold">Any kind</h3>
+        <p className="text-gray-500 text-sm">Of vauation services</p>
+      </div>
+      <div className="flex flex-col items-center text-center w-1/4">
+        <BadgeCheck className="text-blue-500 w-10 h-10 mb-3" />
+        <h3 className="text-xl font-semibold">Licensed Experts</h3>
+        <p className="text-gray-500 text-sm">
+          Certified by national regulatory standards
+        </p>
+      </div>
+      <div className="flex flex-col items-center text-center w-1/4">
+        <Star className="text-blue-500 w-10 h-10 mb-3" />
+        <h3 className="text-xl font-semibold">4.9/5 Client Satisfaction</h3>
+        <p className="text-gray-500 text-sm">Based on 12,500+ reviews</p>
+      </div>
+      <div className="flex flex-col items-center text-center w-1/4">
+        <Handshake className="text-blue-500 w-10 h-10 mb-3" />
+        <h3 className="text-xl font-semibold">Trusted by 50+ agencies</h3>
+        <p className="text-gray-500 text-sm">Based on 12,500+ reviews</p>
+      </div>
     </section>
   );
 }
 
+<Building2 />;
 // რისი საშუალება გექნება ჩვენთან?
 
 // პროფესიონალთა გუნდის მხარდაჭერა
--- src/app/projects/valuation/page.tsx
+++ src/app/projects/valuation/page.tsx
@@ -9,9 +9,25 @@ import TrustSection from "./components/TrustSection";
 // telegrami gasakeTebelia
 
 const bedroomOptions = [1, 2, 3, 4, 5];
+const buildingOptions = ["Apartment", "House", "Office", "Land"];
 
 export default function page() {
   const [bedrooms, setBedrooms] = useState(1);
+  const [buildingType, setBuildingType] = useState("");
+  const [activeHouse, setActiveHouse] = useState(false);
+  const [activeApartment, setActiveApartment] = useState(false);
+  const [acticeOffice, setActiveOffice] = useState(false);
+  const [activeLand, setActiveLand] = useState(false);
+  const [activeAddress, setActiveAddress] = useState(false);
+  const [activeBedrooms, setActiveBedrooms] = useState(false);
+  const [activeLivingArea, setActiveLivingArea] = useState(false);
+  const [activeLivingFloor, setActiveLivingFloor] = useState(false);
+  const activeType = [
+    setActiveApartment,
+    setActiveHouse,
+    setActiveOffice,
+    setActiveLand,
+  ];
   return (
     <LanguageProvider>
       <div className="w-full">
@@ -27,36 +43,51 @@ export default function page() {
               <h1 className="mb-[15px] font-semibold">
                 Enter Property Details
               </h1>
+              <div className="flex gap-6 mb-4">
+                <div className="w-1/2 flex-1">
+                  <label className="block mb-1 font-sm">Property Type</label>
+                </div>
+              </div>
               <div className="mb-4">
+                <div className="flex gap-2">
+                  {buildingOptions.map((n, index) => (
+                    <button
+                      key={n}
+                      onClick={() => {
+                        setBuildingType(n);
+                        activeType[index](true);
+                        activeType.map((n, i) => {
+                          if (i !== index) {
+                            n(false);
+                          }
+                        });
+                      }}
+                      className={`flex-1 py-2 rounded-lg border 
+                  ${
+                    buildingType === n
+                      ? "bg-blue-600 text-white"
+                      : "bg-gray-100"
+                  }`}
+                    >
+                      {n}
+                    </button>
+                  ))}
+                </div>
+              </div>
+              <div className="mb-4 ">
                 <label className="block mb-1 font-sm">Address</label>
                 <input
                   type="text"
                   placeholder="Your Property Address"
-                  className="w-full h-[45px] rounded-lg px-2 py-3 focus:ring-2 focus:ring-blue-500"
+                  className="w-full h-[45px] rounded-lg px-2 py-3 focus:ring-2 focus:ring-blue-500 placeholder:text-xs"
                 ></input>
               </div>
-              <div className="flex gap-6 mb-4">
-                <div className="w-1/2 flex-1">
-                  <label className="block mb-1 font-sm">Property Type</label>
-                  <select className=" rounded-lg px-3 py-3 text-sm">
-                    <option>Select property type</option>
-                    <option>House</option>
-                    <option>Apartment</option>
-                    <option>Land</option>
-                  </select>
-                </div>
-                <div className=" flex-1">
-                  <label className="block mb-1 font-sm">
-                    Living Area (sq ft)
-                  </label>
-                  <input
-                    type="number"
-                    placeholder="e.g. 1500"
-                    className=" rounded-lg h-[44px] px-3 py-3 placeholder:text-sm"
-                  />
-                </div>
-              </div>
-              <div className="mb-4">
+
+              <div
+                className={`mb-4 ${
+                  activeApartment || activeHouse ? "block" : "hidden"
+                }`}
+              >
                 <label className="block mb-2 font-sm">Bedrooms</label>
                 <div className="flex gap-2">
                   {bedroomOptions.map((n) => (
@@ -71,18 +102,103 @@ export default function page() {
                   ))}
                 </div>
               </div>
-              <div>
+              <div
+                className={`flex-1 mb-4 ${
+                  activeApartment || activeHouse || acticeOffice
+                    ? "block"
+                    : "hidden"
+                }`}
+              >
+                <label className="block mb-1 font-sm">
+                  Living Area (sq ft)
+                </label>
+                <input
+                  onInput={(e: React.FormEvent<HTMLInputElement>) => {
+                    // არ მივცეთ "-" ჩაწერის უფლება
+                    const input = e.currentTarget;
+                    if (input.value.includes("-")) {
+                      input.value = input.value.replace(/-/g, "");
+                    }
+
+                    // თუ 0-ზე ნაკლებია → გაუქმდეს
+                    if (Number(input.value) < 0) {
+                      input.value = "0";
+                    }
+                  }}
+                  min="0"
+                  type="number"
+                  placeholder="e.g. 1500"
+                  className=" rounded-lg h-[44px] px-3 py-3 placeholder:text-xs"
+                />
+              </div>
+              <div
+                className={`mb-4 ${
+                  activeApartment || acticeOffice ? "block" : "hidden"
+                }`}
+              >
                 <label>Living floor</label>
                 <input
+                  min="1"
+                  onInput={(e: React.FormEvent<HTMLInputElement>) => {
+                    // არ მივცეთ "-" ჩაწერის უფლება
+                    const input = e.currentTarget;
+                    if (input.value.includes("-")) {
+                      input.value = input.value.replace(/-/g, "");
+                    }
+
+                    // თუ 0-ზე ნაკლებია → გაუქმდეს
+                    if (Number(input.value) < 0) {
+                      input.value = "0";
+                    }
+                  }}
                   type="number"
                   placeholder="e.g. 2"
-                  className="w-full h-[45px] rounded-lg px-3 py-3 "
+                  className="w-full h-[45px] rounded-lg px-3 py-3 placeholder:text-xs"
                 ></input>
               </div>
+              <div className={`flex-1 mb-4 ${activeLand ? "block" : "hidden"}`}>
+                <label className="block mb-1 font-sm">Area Size (sq ft)</label>
+                <input
+                  onInput={(e: React.FormEvent<HTMLInputElement>) => {
+                    // არ მივცეთ "-" ჩაწერის უფლება
+                    const input = e.currentTarget;
+                    if (input.value.includes("-")) {
+                      input.value = input.value.replace(/-/g, "");
+                    }
+
+                    // თუ 0-ზე ნაკლებია → გაუქმდეს
+                    if (Number(input.value) < 0) {
+                      input.value = "0";
+                    }
+                  }}
+                  min="0"
+                  type="number"
+                  placeholder="e.g. 1500"
+                  className=" rounded-lg h-[44px] px-3 py-3 placeholder:text-xs"
+                />
+              </div>
+              <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-lg font-semibold transition ">
+                Get Valuation
+              </button>
+              <p className="text-xs text-gray-500 mt-4 text-center">
+                This is an estimated value for informational purposes only and
+                should not be considered an official appraisal.
+              </p>
             </div>
           </div>
         </div>
       </div>
     </LanguageProvider>
   );
+}
+
+{
+  /* <div className="mb-4">
+  <label className="block mb-1 font-sm">Address</label>
+  <input
+    type="text"
+    placeholder="Your Property Address"
+    className="w-full h-[45px] rounded-lg px-2 py-3 focus:ring-2 focus:ring-blue-500 placeholder:text-xs"
+  ></input>
+</div>; */
 }
