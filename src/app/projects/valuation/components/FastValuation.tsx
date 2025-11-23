40df5954f4bc5c174799585d935ae25a4bf2cae3
giorgi gogichaishvili <g.dodicha@gmail.com>
2025-11-23 23:29:52 +0400

    creating responsive design for any device

---
+++ src/app/projects/valuation/components/FastValuation.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
+const bedroomOptions = [1, 2, 3, 4, 5];
+const buildingOptions = ["Apartment", "House", "Office", "Land"];
+
+export default function FastValuation() {
+  const [bedrooms, setBedrooms] = useState(1);
+  const [buildingType, setBuildingType] = useState("");
+  const [activeHouse, setActiveHouse] = useState(false);
+  const [activeApartment, setActiveApartment] = useState(false);
+  const [acticeOffice, setActiveOffice] = useState(false);
+  const [activeLand, setActiveLand] = useState(false);
+  //   const [activeAddress, setActiveAddress] = useState(false);
+  //   const [activeBedrooms, setActiveBedrooms] = useState(false);
+  //   const [activeLivingArea, setActiveLivingArea] = useState(false);
+  //   const [activeLivingFloor, setActiveLivingFloor] = useState(false);
+  const activeType = [
+    setActiveApartment,
+    setActiveHouse,
+    setActiveOffice,
+    setActiveLand,
+  ];
+  return (
+    <div className="w-full  mr-[30px] text-center">
+      <h2 className="font-semibold text-2xl mb-2">
+        Instant Property Valuation
+      </h2>
+      <div className="w-full rounded-xl py-4 px-4 text-left bg-slate-200">
+        <h1 className="mb-[15px] font-semibold">Enter Property Details</h1>
+        <div className="flex gap-6 mb-4">
+          <div className="w-1/2 flex-1">
+            <label className="block mb-1 font-sm">Property Type</label>
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="flex flex-wrap  gap-2">
+            {buildingOptions.map((n, index) => (
+              <button
+                key={n}
+                onClick={() => {
+                  setBuildingType(n);
+                  activeType[index](true);
+                  activeType.map((n, i) => {
+                    if (i !== index) {
+                      n(false);
+                    }
+                  });
+                }}
+                className={`w-[45%] py-2 rounded-lg border 
+                    ${
+                      buildingType === n
+                        ? "bg-blue-600 text-white"
+                        : "bg-gray-100"
+                    }`}
+              >
+                {n}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="mb-4 ">
+          <label className="block mb-1 font-sm">Address</label>
+          <input
+            type="text"
+            placeholder="Your Property Address"
+            className="w-full h-[45px] rounded-lg px-2 py-3 focus:ring-2 focus:ring-blue-500 placeholder:text-xs"
+          ></input>
+        </div>
+
+        <div
+          className={`mb-4 ${
+            activeApartment || activeHouse ? "block" : "hidden"
+          }`}
+        >
+          <label className="block mb-2 font-sm">Bedrooms</label>
+          <div className="flex gap-2">
+            {bedroomOptions.map((n) => (
+              <button
+                key={n}
+                onClick={() => setBedrooms(n)}
+                className={`flex-1 py-2 rounded-lg border 
+                    ${
+                      bedrooms === n ? "bg-blue-600 text-white" : "bg-gray-100"
+                    }`}
+              >
+                {n}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div
+          className={`flex-1 mb-4 ${
+            activeApartment || activeHouse || acticeOffice ? "block" : "hidden"
+          }`}
+        >
+          <label className="block mb-1 font-sm">Living Area (sq ft)</label>
+          <input
+            onInput={(e: React.FormEvent<HTMLInputElement>) => {
+              // არ მივცეთ "-" ჩაწერის უფლება
+              const input = e.currentTarget;
+              if (input.value.includes("-")) {
+                input.value = input.value.replace(/-/g, "");
+              }
+
+              // თუ 0-ზე ნაკლებია → გაუქმდეს
+              if (Number(input.value) < 0) {
+                input.value = "0";
+              }
+            }}
+            min="0"
+            type="number"
+            placeholder="e.g. 1500"
+            className=" rounded-lg h-[44px] px-3 py-3 placeholder:text-xs"
+          />
+        </div>
+        <div
+          className={`mb-4 ${
+            activeApartment || acticeOffice ? "block" : "hidden"
+          }`}
+        >
+          <label>Living floor</label>
+          <input
+            min="1"
+            onInput={(e: React.FormEvent<HTMLInputElement>) => {
+              // არ მივცეთ "-" ჩაწერის უფლება
+              const input = e.currentTarget;
+              if (input.value.includes("-")) {
+                input.value = input.value.replace(/-/g, "");
+              }
+
+              // თუ 0-ზე ნაკლებია → გაუქმდეს
+              if (Number(input.value) < 0) {
+                input.value = "0";
+              }
+            }}
+            type="number"
+            placeholder="e.g. 2"
+            className="w-full h-[45px] rounded-lg px-3 py-3 placeholder:text-xs"
+          ></input>
+        </div>
+        <div className={`flex-1 mb-4 ${activeLand ? "block" : "hidden"}`}>
+          <label className="block mb-1 font-sm">Area Size (sq ft)</label>
+          <input
+            onInput={(e: React.FormEvent<HTMLInputElement>) => {
+              // არ მივცეთ "-" ჩაწერის უფლება
+              const input = e.currentTarget;
+              if (input.value.includes("-")) {
+                input.value = input.value.replace(/-/g, "");
+              }
+
+              // თუ 0-ზე ნაკლებია → გაუქმდეს
+              if (Number(input.value) < 0) {
+                input.value = "0";
+              }
+            }}
+            min="0"
+            type="number"
+            placeholder="e.g. 1500"
+            className=" rounded-lg h-[44px] px-3 py-3 placeholder:text-xs"
+          />
+        </div>
+        <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-lg font-semibold transition ">
+          Get Valuation
+        </button>
+        <p className="text-xs text-gray-500 mt-4 text-center">
+          This is an estimated value for informational purposes only and should
+          not be considered an official appraisal.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/projects/valuation/components/TrustSection.tsx
+++ src/app/projects/valuation/components/TrustSection.tsx
@@ -2,57 +2,65 @@
 
 import React from "react";
 import { useLanguage } from "../context/LanguageProvider";
-import { Award, Building2, BadgeCheck, Star, Handshake } from "lucide-react";
+import {
+  Award,
+  Building2,
+  BadgeCheck,
+  Star,
+  Handshake,
+  House,
+} from "lucide-react";
+
+const icons = [
+  {
+    component: Building2,
+    title: "35,000+",
+    description: "properties valuated",
+  },
+  { component: Award, title: "17+ years", description: "of work experience" },
+  {
+    component: House,
+    title: "Any kind",
+    description: "Of vauation services",
+  },
+  {
+    component: BadgeCheck,
+    title: "Licensed Experts",
+    description: "Certified by national regulatory standards",
+  },
+  {
+    component: Star,
+    title: "4.9/5 Client Satisfaction",
+    description: "Based on 12,500+ reviews",
+  },
+  {
+    component: Handshake,
+    title: "Trusted by 50+ agencies",
+    description: "Based on 12,500+ reviews",
+  },
+];
 
 export default function TrustSection() {
   const { selectedLanguageData } = useLanguage();
+
+  const trustSectionComponents = icons.map((item, index) => {
+    const Icon = item.component;
+    return (
+      <div className="flex flex-col items-center   text-center w-2/5 bg-red-500 ">
+        <Icon className="text-blue-500  w-10 h-10 mb-3" />
+        <h3 className="text-xl font-semibold">{item.title}</h3>
+        <p className="text-gray-500 text-sm">{item.description}</p>
+      </div>
+    );
+  });
+
   return (
-    <section className=" w-4/5 ml-[50px]  mr-4 flex flex-wrap gap-8 p-24">
-      {/* 1 */}
-      <div className="flex flex-col items-center text-center w-1/4 ">
-        <Building2 className="text-blue-500 w-10 h-10 mb-3" />
-        <h3 className="text-xl font-semibold">35,000+</h3>
-        <p className="text-gray-500 text-sm">
-          {selectedLanguageData?.["properties valuated"] ?? ""}
-        </p>
-      </div>
-      {/* 2 */}
-      <div className="flex flex-col items-center text-center w-1/4">
-        <Award className="text-blue-500 w-10 h-10 mb-3" />
-        <h3 className="text-xl font-semibold">
-          17+ <span>{selectedLanguageData?.["years"]}</span>{" "}
-        </h3>
-        <p className="text-gray-500 text-sm">
-          {selectedLanguageData?.["of work experience"]}
-        </p>
-      </div>
-      <div className="flex flex-col items-center text-center w-1/4">
-        <Building2 className="text-blue-500 w-10 h-10 mb-3" />
-        <h3 className="text-xl font-semibold">Any kind</h3>
-        <p className="text-gray-500 text-sm">Of vauation services</p>
-      </div>
-      <div className="flex flex-col items-center text-center w-1/4">
-        <BadgeCheck className="text-blue-500 w-10 h-10 mb-3" />
-        <h3 className="text-xl font-semibold">Licensed Experts</h3>
-        <p className="text-gray-500 text-sm">
-          Certified by national regulatory standards
-        </p>
-      </div>
-      <div className="flex flex-col items-center text-center w-1/4">
-        <Star className="text-blue-500 w-10 h-10 mb-3" />
-        <h3 className="text-xl font-semibold">4.9/5 Client Satisfaction</h3>
-        <p className="text-gray-500 text-sm">Based on 12,500+ reviews</p>
-      </div>
-      <div className="flex flex-col items-center text-center w-1/4">
-        <Handshake className="text-blue-500 w-10 h-10 mb-3" />
-        <h3 className="text-xl font-semibold">Trusted by 50+ agencies</h3>
-        <p className="text-gray-500 text-sm">Based on 12,500+ reviews</p>
-      </div>
+    <section className=" w-4/5 bg-black ml-[30px] mt-12 justify-center h-min mr-4 flex flex-wrap gap-6 ">
+      {trustSectionComponents}
     </section>
   );
 }
 
-<Building2 />;
 // რისი საშუალება გექნება ჩვენთან?
 
 // პროფესიონალთა გუნდის მხარდაჭერა
